fc76da4c607946fd2534c5a6b50505f4fe532481
Tayman <taymantott@gmail.com>
2024-05-13 13:44:12 -0500

    Update followUser.js

--- followUser.js
+++ followUser.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
 	user: "postgres",
-	password: "mercury666",
+	password: "password",
 	database: "test",
 	host: "localhost",
 	port: "5432"
